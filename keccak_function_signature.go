6105f07ced9b64468b515dc3db054a8cebd05779
DrewZ <zty749@sina.com>
2020-05-27 11:47:01 +0800

    add error handling

--- keccak_function_signature.go
+++ keccak_function_signature.go
@@ -11,17 +11,25 @@ func parseString(s string) ([]byte, error) {
 	var b []byte
 	b = []byte(s)
 	hash := sha3.NewLegacyKeccak256()
-	_, _ = hash.Write(b)
+	_, err := hash.Write(b)
+	if err != nil {
+		return nil, err
+	}
 	return hash.Sum(nil), nil
 }
 
 func IndexHandler(rw http.ResponseWriter, req *http.Request) {
 	str := req.URL.Path
 	content := str[1:]
-	var a []byte
-	a, _ = parseString(content)
+	a, err := parseString(content)
+	if err != nil {
+		fmt.Println(err)
+	}
 	s1 := a[0:4]
-	_, _ = fmt.Fprint(rw, hex.EncodeToString(s1))
+	_, err = fmt.Fprint(rw, hex.EncodeToString(s1))
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func main() {
